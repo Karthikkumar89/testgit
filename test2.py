71d629c2d4395f454bbc3b642d66a4e9f2322ca0
kakms <skarthik.nitt@gmail.com>
2022-07-29 20:21:38 +0530

    This is my first code for push

---
+++ test2.py
@@ -0,0 +1,16 @@
+import pymongo
+import dns
+
+client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.lh6u7hp.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+d={
+    "name":"karthik",
+    "age":32,
+    "surname":"kumar"
+
+}
+
+db1=client['mongotest']
+coll=db1["test"]
+coll.insert_one(d)
